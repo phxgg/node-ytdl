d16d5f25009c6d215fc8df89f48720ad8184314d
phxgg <stamatis.1702@gmail.com>
2020-05-17 17:44:22 +0300

    new

--- public/js/main.js
+++ public/js/main.js
@@ -54,70 +54,7 @@ $('#btnConvert').click(function(e) {
     });
 });
 
-$('input[name="url"]').on('input', function () {
-    var url = $(this).val();
-
-    if (helper.isValidYtLink(url)) {
-        $('#yt-loading').show();
-
-        var csrf    = $('input[name="_csrf"]').val();
-        var sid     = socketId;
-
-        $.ajax({
-            url: '/videoinfo',
-            data: {
-                _csrf       : csrf,
-                socketId    : sid,
-                url         : url
-            },
-            dataType: 'JSON',
-            method: 'POST',
-
-            success: function(res, status, xhr) {
-                var statusCode = res.statusCode;
-                var info = res.info;
-
-                switch (statusCode) {
-                    case 'error':
-                        $('#yt-videoinfo').hide();
-                        $('#url').addClass('is-invalid');
-                        $('#labelInvalidLink').show();
-                        break;
-                    case 'success':
-                        $('#yt-thumbnail').html(`<a href="${url}" target="_blank"><img src="${info.thumbnail.url}"></a>`);
-                        $('#yt-title').html(`${info.title}`);
-
-                        var likes = parseInt(info.likes);
-                        var dislikes = parseInt(info.dislikes);
-
-                        var dislikePercentage = Math.round(dislikes/likes * 100);
-                        var likePercentage = 100 - dislikePercentage;
-
-                        $('#url').removeClass('is-invalid');
-                        $('#labelInvalidLink').hide();
-
-                        $('#yt-views').html(`Views: ${helper.num(info.views)}`);
-
-                        $('#yt-likeperc').css('width', `${likePercentage}%`);
-                        $('#yt-likeperc').attr('aria-valuenow', `${likePercentage}`);
-                        $('#yt-likeperc').attr('data-original-title', `${helper.num(likes)} Likes`);
-
-                        $('#yt-dislikeperc').css('width', `${dislikePercentage}%`);
-                        $('#yt-dislikeperc').attr('aria-valuenow', `${dislikePercentage}`);
-                        $('#yt-dislikeperc').attr('data-original-title', `${helper.num(dislikes)} Dislikes`);
-
-                        $('#yt-likes').html(`${helper.num(info.likes)}`);
-                        $('#yt-dislikes').html(`${helper.num(info.dislikes)}`);
-
-                        $('#yt-videoinfo').show();
-                        break;
-                }
-
-                $('#yt-loading').hide();
-            }
-        });
-    }
-});
+// url on key change
 
 $('#btnContact').click(function (e) {
     e.preventDefault();
