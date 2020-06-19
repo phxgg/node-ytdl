987b1953ba5a73f34d762916441aecadf6b18e34
phxgg <stamatis.1702@gmail.com>
2020-06-19 04:49:56 +0300

    new

--- public/js/helper.js
+++ public/js/helper.js
@@ -29,3 +29,42 @@ var helper = {
         return Number(num).toLocaleString('el-GR');
     }
 };
+
+function setCookie(name, value, days) { // from http://www.quirksmode.org/js/cookies.html
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+window.onload = function () {
+    var quality = getCookie('qualityCookie');
+
+    if (quality) {
+        if (quality == "mp3low") $('#customRadio1').click(); // or .checked=true; 
+        else if (quality == "mp3best") $('#customRadio2').click();
+    }
+
+    $('#customRadio').click(function() {
+        var radioValue = $("input[name='quality']:checked").val();
+        setCookie('qualityCookie', radioValue, 30);
+    });
+
+    $('#customRadio2').click(function() {
+        var radioValue = $("input[name='quality']:checked").val();
+        setCookie('qualityCookie', radioValue, 30);
+    });
+}
