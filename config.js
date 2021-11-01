e5f227c0798926ba34e8d00cca1737cdb3a73a3b
phxgg <localhost>
2021-11-02 01:12:57 +0200

    new

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ config.discord.prefix           = '++';
 config.discord.token            = 'Njg0NzQ0MDk2OTM3NDEwNjE1.Xl-kEA.kGDDt2XagSXXxsrSCsR_p7WHb4k';
 
 // Web Settings
-// config.web.site                 = 'https://node-ytdl.herokuapp.com';
-config.web.site                 = 'http://localhost:4000';
+config.web.site                 = 'https://node-ytdl.herokuapp.com';
+// config.web.site                 = 'http://localhost:4000';
 config.web.port                 = process.env.PORT || 4000;
 config.web.sessionKey           = 'Dbb1Qp8HiS56Rwlj8jpJ';
 
